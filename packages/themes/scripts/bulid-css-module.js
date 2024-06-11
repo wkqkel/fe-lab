44d4f1004a3a45e26f0909f43fbb7e1a542f6be6
sangwon <sangwon@sangwonui-MacBookPro.local>
2024-06-11 17:12:26 +0900

    typography 토큰

--- packages/themes/scripts/bulid-css-module.js
+++ packages/themes/scripts/bulid-css-module.js
@@ -6,6 +6,12 @@ import fs from 'fs'
  * :root {
  *  --gray-900: #171923
  * }
+ * 
+ * .headingxl {
+ *  font-size: 3rem;
+ *  font-weight: 700;
+ *  line-height: 100%;
+ * }
  */
 
 const toCssCasting = (str) => {
@@ -30,17 +36,51 @@ const generateThemeCssVariables = () => {
 
                 cssString.push(`${selector} {\n${cssVariables}\n}`)
             })
+            return
         }
 
+
+        const selector = ":root"
+
+        const cssVariables = Object.entries(value).map(([mainKey, mainValue]) => (
+            Object.entries(mainValue).map(([subKey, subValue]) => `--${toCssCasting(mainKey)}-${toCssCasting(subKey)}: ${subValue};`)
+        ).join('\n')).join('\n')
+
+
+        cssString.push(`${selector} {\n${cssVariables}\n}`)
     })
 
     return cssString
 }
 
+const generateThemeCssClasses = () => {
+    const cssString = []
+
+    Object.entries(theme.classes).forEach(([, value]) => {
+        const cssClasses = Object.entries(value).map(([mainKey, mainValue]) => (
+
+            Object.entries(mainValue).map(([subKey, subValue]) => {
+                const className = `.${toCssCasting(mainKey)}${toCssCasting(subKey)}`
+
+                const styleProperties = Object.entries(subValue).map(([styleKey, styleValue]) => {
+                    return `${toCssCasting(styleKey)}: ${styleValue};`
+                }).join('\n')
+
+                return `${className} {\n${styleProperties}\n}`
+            }).join('\n')
+        )).join('\n')
+
+        cssString.push(cssClasses)
+    })
+
+    return cssString;
+}
+
 const generateThemeCss = () => {
     const variables = generateThemeCssVariables();
+    const classes = generateThemeCssClasses();
 
-    fs.writeFileSync('dist/theme.css', variables.join('\n'))
+    fs.writeFileSync('dist/theme.css', [...variables, ...classes].join('\n'))
 }
 
 generateThemeCss()
